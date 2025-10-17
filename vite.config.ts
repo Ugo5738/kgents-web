1884e18fc4d54d8a20eebf0f4d0b08633b00c100
daniel <danielnwachukwu5738@gmail.com>
2025-10-17 07:53:17 +0100

    Set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
+import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => {
       },
       plugins: [react()],
       base: '/kgents-web/',  
+      // base: './',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
